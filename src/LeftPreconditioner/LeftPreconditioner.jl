8480b38aa6358d36c781afbe7e56fac7d027dac5
Francis <ziyi.yin.97@gmail.com>
2020-11-10 11:40:47 -0500

    half integration

--- src/LeftPreconditioner/LeftPreconditioner.jl
+++ src/LeftPreconditioner/LeftPreconditioner.jl
@@ -9,11 +9,30 @@ export LeftPrecondJ
 # where P1 is fractional integration
 
 function LeftPrecondJ(J::judiJacobian)
-	
+	nsrc = length(J.srcGeometry.xloc)
 	nt = J.recGeometry.nt[1]
+
 	x    = zeros(Int64,nt,1);
 	x[1] = 1;
 	y    = convert(Array{Float32,2},fgl_deriv(0.5,x,1));	# half integration
-	C    = joConvolve(nt,1,y[:]);
-	return C
+	C    = joConvolve(nt,1,y[:];DDT=Float32,RDT=Float32);
+
+	P = joLinearFunctionFwd_T(prod(n), prod(n),
+	                             v -> integral_shot(C,v),
+	                             w -> integral_shot(C',w),
+								 Float32,Float32,name="Fractional integration operator")
 end
+
+function integral_shot(C::joLinearFunction,d_obs::judiVector)
+	d_out = deepcopy(d_obs)
+	nrec = length(d_obs.geometry.xloc[1])
+	nt = d_obs.geometry.nt[1]
+	nsrc = d_obs.nsrc
+	for i = 1:nsrc
+		for j = 1:nrec
+			d_out.data[i][:,j] = C*d_lin.data[i][:,j]
+		end
+	end
+	return d_out
+end
+
