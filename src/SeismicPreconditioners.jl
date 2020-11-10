3265089ba71ad7800736d8696569958f1a488c00
Francis <ziyi.yin.97@gmail.com>
2020-11-10 10:48:32 -0500

    special functions

--- src/SeismicPreconditioners.jl
+++ src/SeismicPreconditioners.jl
@@ -1,7 +1,7 @@
 __precompile__()
 module SeismicPreconditioners
 
-using JUDI.TimeModeling, JOLI, LinearAlgebra, FFTW, IterativeSolvers
+using JUDI.TimeModeling, JOLI, LinearAlgebra, FFTW, IterativeSolvers, SpecialFunctions
 
 include("RightPreconditioner/RightPreconditioner.jl")
 
